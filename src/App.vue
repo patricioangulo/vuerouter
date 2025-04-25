a37fedcbc9efde37633bfbcdfab6e592163cfde0
Patricio Angulo <kamefront@gmail.com>
2025-04-25 17:54:41 +1000

    add prime vue

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,10 @@
 <script setup lang="ts">
-import Navigation from './components/Navigation.vue'
+import Navigation from './components/Navigation.vue';
+import Button from 'primevue/button';
 </script>
 
 <template>
-  <Navigation/>
-  <router-view/>
+  <Navigation />
+  <router-view />
+  <Button>See</Button>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,23 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
+import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura';
+import Lara from '@primeuix/themes/lara';
+import Nora from '@primeuix/themes/nora';
 
-createApp(App).use(router).mount('#app')
+createApp(App)
+  .use(router)
+  .use(PrimeVue,
+    {
+      theme: {
+        preset: Lara,
+        options: {
+          prefix: 'p',
+          darkModeSelector: 'system',
+          cssLayer: false
+        }
+      }
+    }
+  )
+  .mount('#app')
